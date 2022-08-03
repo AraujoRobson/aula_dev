7cb700d638f859fc3607c166008962b02afb573c
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-03 17:53:49 -0300

    atualizar a quantidade do estoque

--- AtividadeFinal/src/model/Venda.java
+++ AtividadeFinal/src/model/Venda.java
@@ -4,26 +4,27 @@ import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
-public class Venda {
+public class Venda{
 	private double vlrTotal = 0;
-	private String itens = "";
 	Estoque estoque = new Estoque();
 	
 	ArrayList<Produto> listaVenda = new ArrayList<>();
 	
-	public void addItemVenda(Produto produto, int qtdParaVenda) {
-		if(estoque.verificarEstoque(produto, qtdParaVenda)) {
+	public void addItemVenda(Produto produto, int qtdParaVenda) {	
+		if(produto.verificarEstoque(produto, qtdParaVenda)) {
 			for(int i = 0; i < qtdParaVenda; i++) {
 				listaVenda.add(produto);
 			}
-			produto.setPreco((produto.getQtdEstoque() - qtdParaVenda));
+			produto.setQtdEstoque((produto.getQtdEstoque() - qtdParaVenda));
 		}else {
 			JOptionPane.showMessageDialog(null, "Estoque insuficiente!");
 		}
 	}
 	public String visualizarVenda() {
+		String itens = "";
+		vlrTotal = 0;
 		for(int i = 0; i < listaVenda.size(); i++) {
-			itens = String.format("%s\n%s		R$%.2f", itens,listaVenda.get(i).getNome(),listaVenda.get(i).getPreco());
+			itens = String.format("%s\n%s		R$%.2f", itens, listaVenda.get(i).getNome(), listaVenda.get(i).getPreco());
 			vlrTotal += listaVenda.get(i).getPreco();
 		}
 		return String.format("\n%s\nValor total: R$%.2f", itens , vlrTotal);
