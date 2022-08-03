e97f4db3c67443cbf1c1bb1562f116cf6a0b58c8
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-03 14:44:10 -0300

    add classe estoque

--- AtividadeFinal/src/model/Estoque.java
+++ AtividadeFinal/src/model/Estoque.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Estoque {
-	private double vlrTotal = 0;
-	private String listaItens = "";
 	ArrayList<Produto> estoque = new ArrayList<>();
 	
 	public void addItem(Produto produto, int qtdParaVenda) {
@@ -20,10 +18,8 @@ public class Estoque {
 		}
 	}
 	
-	
-	
 	public String visualizarEstoque() {
-		listaItens = "";
+		String listaItens = "";
 		for(int i = 0; i < estoque.size(); i++) {
 			listaItens = String.format("%s\nCod:%d	 | %s	| Qtd: %d	| R$%.2f", listaItens, (i + 1),estoque.get(i).getNome(), estoque.get(i).getQtdEstoque(), estoque.get(i).getPreco());
 		}
@@ -32,16 +28,10 @@ public class Estoque {
 	
 	public String[] nomes() {
         String[] lista = new String[estoque.size()];
-
         for(int i = 0; i < estoque.size(); i++) {
             lista[i] = estoque.get(i).getNome();
         }
-
         return lista;
     }
 	
-	public String concluirVenda(Pagamento pag) {
-		
-		return String.format("Valor total: R$%.2f\n%s", vlrTotal, pag.realizarPagamento());
-	}
 }
