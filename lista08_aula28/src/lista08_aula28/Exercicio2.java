297337028c4100c875539425e7f826afddae9a71
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-06 14:49:52 -0300

    reforçando utilização de casting

---
+++ lista08_aula28/src/lista08_aula28/Exercicio2.java
@@ -0,0 +1,11 @@
+package lista08_aula28;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        double numero1 = 1234.56;
+        float numero2 = (float) numero1;
+
+        int numero3 = 12;
+        byte numero4 = (byte) numero3;
+    }
+}
