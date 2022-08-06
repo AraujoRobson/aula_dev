4fc608a18aebd1fb512fd718ee019057a49a3cb2
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-06 16:08:16 -0300

    refatorar codigo e printar dados

---
+++ lista08_aula28/src/lista08_aula28/Exercicio7.java
@@ -0,0 +1,14 @@
+package lista08_aula28;
+
+public class Exercicio7 {
+    final static double PI = 3.1415;
+    final static long NUMERO = 999_999L;
+
+    public static void main(String[] args) {
+        final String NOME = "DEV-TI";
+
+        System.out.println("valor de PI: " + PI);
+        System.out.println("Valor long: " + NUMERO);
+        System.out.println("valor nome: " + NOME);
+    }
+}
