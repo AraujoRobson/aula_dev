e0e5b994ac685c10f8270c6a8359d865102f3c77
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-06 15:47:44 -0300

    uso de entrada de dados e if else

---
+++ lista08_aula28/src/lista08_aula28/Exercicio4.java
@@ -0,0 +1,28 @@
+package lista08_aula28;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Entre com seu peso: ");
+        float peso = scan.nextFloat();
+        System.out.println("Entre com sua altura: (em metros)");
+        float altura = scan.nextFloat();
+
+        float imc = peso * (altura * altura);
+        String resultado;
+        if (imc < 18.5) {
+            resultado = "Classificação: MAGREZA";
+        } else if (imc <= 24.9) {
+            resultado = "Classificação: NORMAL";
+        } else if (imc <= 29.9) {
+            resultado = "Classificação: SOBREPESO";
+        } else if (imc <= 39.9) {
+            resultado = "Classificação: OBESIDADE";
+        } else {
+            resultado = "Classificação: OBESIDADE GRAVE";
+        }
+        System.out.println("O IMC é: " + String.format("%.2f", imc) + "\n" + resultado);
+    }
+}
