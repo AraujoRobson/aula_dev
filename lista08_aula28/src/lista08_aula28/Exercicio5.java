7ed511a98bb73c78f44742af1e04e092fe06fa77
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-06 15:55:08 -0300

    refatorar codigo do exercicio

--- lista08_aula28/src/lista08_aula28/Exercicio5.java
+++ lista08_aula28/src/lista08_aula28/Exercicio5.java
@@ -2,6 +2,8 @@ package lista08_aula28;
 
 public class Exercicio5 {
     public static void main(String[] args) {
-
+        int raio = 5000;
+        int circunferencia = (int) (2 * Math.PI * raio);
+        System.out.println(circunferencia);
     }
 }
