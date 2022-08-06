6ad7854adf27ae7f6ac47916292cbdf3c679609a
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-06 16:22:40 -0300

    implementando autoboxing var primitiva to classe wrapper

--- lista08_aula28/src/lista08_aula28/Exercicio8.java
+++ lista08_aula28/src/lista08_aula28/Exercicio8.java
@@ -2,6 +2,16 @@ package lista08_aula28;
 
 public class Exercicio8 {
     public static void main(String[] args) {
+        int num1 = 11;
+        float num2 = 22.22f;
+        double num3 = 33.33;
 
+        Integer w1 = Integer.valueOf(num1);
+        Float w2 = Float.valueOf(num2);
+        Double w3 = Double.valueOf(num3);
+
+        System.out.println("Integer: " + w1);
+        System.out.println("Float: " + w2);
+        System.out.println("Double: " + w3);
     }
 }
