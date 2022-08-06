1e572bcb025c0df4903ce594680591340e6c5056
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-06 17:09:20 -0300

    ajuste de telefones

--- lista08_aula28/src/hotel_Exercicio11/Principal.java
+++ lista08_aula28/src/hotel_Exercicio11/Principal.java
@@ -14,7 +14,7 @@ public class Principal {
 
 		System.out.println("GERENTE");
 		System.out.println();
-		Gerente gerente = new Gerente("TEOBALDO", "4998784212", "548978916");
+		Gerente gerente = new Gerente("JÉSSICA", "49 99997070", "548978916");
 		gerente.arrumarACama();
 		gerente.arrumarCozinha();
 		gerente.atender();
@@ -25,7 +25,7 @@ public class Principal {
 
 		System.out.println("RECEPCIONISTA");
 		System.out.println();
-		Recepcionista recepcionista = new Recepcionista("REB�CA", "4987978978", "4868789798789");
+		Recepcionista recepcionista = new Recepcionista("PEDRO", "49 99997070", "4868789798789");
 		recepcionista.atender();
 		recepcionista.falarIngles();
 		System.out.println(recepcionista.toString());
@@ -33,14 +33,14 @@ public class Principal {
 
 		System.out.println("CAMAREIRA");
 		System.out.println();
-		Camareira camareira = new Camareira("LARITNA", "4999889898", "787975678789");
+		Camareira camareira = new Camareira("LARITNA", "49 99997070", "787975678789");
 		camareira.arrumarACama();
 		System.out.println(camareira.toString());
 		System.out.println();
 
 		System.out.println("COZINHEIRA");
 		System.out.println();
-		Cozinheira cozinheira = new Cozinheira("ROSEVITA", "45648978965", "7954564678");
+		Cozinheira cozinheira = new Cozinheira("ROSEVITA", "49 99997070", "7954564678");
 		cozinheira.Cozinhar();
 		cozinheira.arrumarCozinha();
 		System.out.println(cozinheira.toString());
@@ -48,22 +48,14 @@ public class Principal {
 
 		System.out.println("FAZ TUDO");
 		System.out.println();
-		FazTudo faztudo = new FazTudo("BATISTA", "45645649879", "45678978979");
-		faztudo.FazTudo();
+		FazTudo faztudo = new FazTudo("BATISTA", "49 99997070", "45678978979");
+		faztudo.cafe();
 		System.out.println(faztudo.toString());
-		System.out.println();
-
-		System.out.println("AUXILIAR GERAL");
-		System.out.println();
-		AuxiliarGeral auxiliargeral = new AuxiliarGeral("EUST�QUIO", "45678979787", "42564645646");
-		auxiliargeral.AuxiliarGeral();
-		System.out.println(auxiliargeral.toString());
 		System.out.println();
 
 		System.out.println("CLIENTE");
 		System.out.println();
-		Cliente cliente = new Cliente("ADRIAN", "49991513025", "10578351960", 32000);
+		Cliente cliente = new Cliente("ADRIAN", "49 99997070", "10578351960", 32000);
 		System.out.println(cliente.toString());
 	}
-
 }
