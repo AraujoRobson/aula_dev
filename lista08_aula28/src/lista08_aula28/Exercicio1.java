8979f58ffa7ffd8da07517d222388a6e332264cb
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-06 14:34:48 -0300

    add aula 28

---
+++ lista08_aula28/src/lista08_aula28/Exercicio1.java
@@ -0,0 +1,11 @@
+package lista08_aula28;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		int numero = 12;
+		numero = (byte) numero;
+		System.out.println(numero);
+	}
+
+}
