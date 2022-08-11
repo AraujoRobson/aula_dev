3119c48e8411d3b923fff401e0122e826241cadf
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-11 20:20:40 -0300

    criando varios erros para criar tratamento

---
+++ aula29/src/problemas/Problemas.java
@@ -0,0 +1,41 @@
+package problemas;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Problemas {
+	public static void main(String[] args) {
+		int[] arranjo = { 10, 20, 30 };
+		System.out.println("Array:");
+		System.out.println(arranjo[0]);
+		System.out.println(arranjo[3]);
+		// java.lang.ArrayIndexOutOfBoundsException:
+
+		ArrayList<Integer> numeros = new ArrayList<>();
+		numeros.add(10);
+		numeros.add(20);
+		numeros.add(30);
+		System.out.println("\nLista:");
+		System.out.println(numeros.get(0));
+		System.out.println(numeros.get(3));
+		// java.lang.IndexOutOfBoundsException:
+
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Entre com um numero decimal: 'com ponto'");
+		double numero = scan.nextDouble();
+		// java.util.InputMismatchException
+		scan.close();
+		numero = Double.parseDouble("10,25");
+		// convertendo com virugula
+		// java.lang.NumberFormatException
+
+		System.out.println(10 / 0);
+		// java.lang.ArithmeticException
+
+		String frase = null;
+		System.out.println(frase.toUpperCase());
+		// java.lang.NullPointerException
+	}
+}
