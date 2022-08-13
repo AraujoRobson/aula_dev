58fd3addb5b69c8df8f2e4cadde4dc5bcd649bf6
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-12 22:32:00 -0300

    testes personalizados

--- aula30Testes/src/testesautomatizados/CalculadoraTest.java
+++ aula30Testes/src/testesautomatizados/CalculadoraTest.java
@@ -24,4 +24,18 @@ public class CalculadoraTest {
 		int soma = calc.somar(10, 0);
 		Assert.assertEquals(10, soma);
 	}
+
+	@Test
+	public void deveriaSomarUmNumeroPositivoComNumeroNegativo() {
+		Calculadora calc = new Calculadora();
+		int soma = calc.somar(5, -5);
+		Assert.assertEquals(0, soma);
+	}
+	
+	@Test
+	public void deveriaSomarDoisNumerosNegativos() {
+		Calculadora calc = new Calculadora();
+		int soma = calc.somar(-2, -3);
+		Assert.assertEquals(-5, soma);
+	}
 }
