84e5883ad445822574567ef7351073cbcf438e9c
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-13 10:21:01 -0300

    erros tratados

--- lista9/src/exercicios1/Ex1a.java
+++ lista9/src/exercicios1/Ex1a.java
@@ -4,13 +4,16 @@ public class Ex1a {
 
 	public static void main(String[] args) {
 		try {
-			int[] numeros = {10, 0};
-			
-			System.out.println(numeros[0] / numeros[2]);
+			int[] numeros = { 10, 0 };
+			System.out.println(numeros[0] / numeros[1]);
+		} catch (ArithmeticException e) {
+			System.out.println("Impossivel dividir por 0");
+		} catch (RuntimeException e) {
+			System.out.println("Erro de Runtime.");
+			System.out.println(e.getMessage());
 		} catch (Exception e) {
 			System.out.println("Erro encontrado!");
 			System.out.println(e.getMessage());
 		}
 	}
-
 }
