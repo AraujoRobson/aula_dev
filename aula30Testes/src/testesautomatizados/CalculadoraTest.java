f244bc8a7a311c8ba475e4e4f45879d56efcaaa7
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-15 20:18:23 -0300

    testes para subtrair

--- aula30Testes/src/testesautomatizados/CalculadoraTest.java
+++ aula30Testes/src/testesautomatizados/CalculadoraTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import testesmanuais.Calculadora;
 
 public class CalculadoraTest {
+	// SOMAR
 	@Test
 	public void deveriaSomarDoisNumerosPositivos() {
 		// Cenario (arrange)
@@ -37,5 +38,55 @@ public class CalculadoraTest {
 		Calculadora calc = new Calculadora();
 		int soma = calc.somar(-2, -3);
 		assertEquals(-5, soma);
+	}
+
+	// SUBTRAIR
+	@Test
+	public void deveriaSubtrairDoisNumerosPositivos() {
+		Calculadora calc = new Calculadora();
+		int subtrair = calc.subtrair(20, 5);
+		assertEquals(15, subtrair);
+	}
+
+	@Test
+	public void deveriaSubtrairUmNumeroNegativoPorUmPositivo() {
+		Calculadora calc = new Calculadora();
+		int subtrair = calc.subtrair(-20, 5);
+		assertEquals(-25, subtrair);
+	}
+
+	@Test
+	public void deveriaSubtrairUmNumeroPositivoPorUmNegtivo() {
+		Calculadora calc = new Calculadora();
+		int subtrair = calc.subtrair(22, -5);
+		assertEquals(27, subtrair);
+	}
+
+	@Test
+	public void deveriaSubtrairDoisNumerosNegativos() {
+		Calculadora calc = new Calculadora();
+		int subtrair = calc.subtrair(-40, -15);
+		assertEquals(-25, subtrair);
+	}
+
+	@Test
+	public void deveriaSubtrairUmNumeroPositivoPorZero() {
+		Calculadora calc = new Calculadora();
+		int subtrair = calc.subtrair(18, 0);
+		assertEquals(18, subtrair);
+	}
+
+	@Test
+	public void deveriaSubtrairZeroPorUmNumeroPositivo() {
+		Calculadora calc = new Calculadora();
+		int subtrair = calc.subtrair(0, 22);
+		assertEquals(-22, subtrair);
+	}
+
+	@Test
+	public void deveriaSubtrairZeroPorUmNumeroNegativo() {
+		Calculadora calc = new Calculadora();
+		int subtrair = calc.subtrair(0, -22);
+		assertEquals(22, subtrair);
 	}
 }
