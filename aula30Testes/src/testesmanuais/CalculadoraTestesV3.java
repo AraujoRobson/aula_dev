3f432b6a03b4b2f02878a0658976c851d87c3bef
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-15 20:00:44 -0300

    correcao

--- aula30Testes/src/testesmanuais/CalculadoraTestesV3.java
+++ aula30Testes/src/testesmanuais/CalculadoraTestesV3.java
@@ -7,27 +7,27 @@ public class CalculadoraTestesV3 {
 
 		soma = calc.somar(41, 1);
 		if (soma == 42) {
-			System.out.println("Resultado " + soma + " est� correto!");
+			System.out.println("Resultado " + soma + " está correto!");
 		}
 
 		soma = calc.somar(10, 0);
 		if (soma == 10) {
-			System.out.println("Resultado " + soma + " est� correto!");
+			System.out.println("Resultado " + soma + " está correto!");
 		}
 
 		soma = calc.somar(5, -5);
 		if (soma == 0) {
-			System.out.println("Resultado " + soma + " est� correto!");
+			System.out.println("Resultado " + soma + " está correto!");
 		}
 
 		soma = calc.somar(-2, -3);
 		if (soma == -5) {
-			System.out.println("Resultado " + soma + " est� correto!");
+			System.out.println("Resultado " + soma + " está correto!");
 		}
 
 		soma = calc.somar(2147483647, 1);
 		if (soma == 2147483648L) {
-			System.out.println("Resultado " + soma + " est� correto!");
+			System.out.println("Resultado " + soma + " está correto!");
 		}
 	}
 }
