2f245490a90c47ce93b9c337747797b857ec5c87
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-15 20:00:25 -0300

    correcao

--- aula30Testes/src/testesmanuais/CalculadoraTestesV4.java
+++ aula30Testes/src/testesmanuais/CalculadoraTestesV4.java
@@ -7,35 +7,35 @@ public class CalculadoraTestesV4 {
 
 		soma = calc.somar(41, 1);
 		if (soma == 42) {
-			System.out.println("Resultado " + soma + " est· correto!");
+			System.out.println("Resultado " + soma + " est√° correto!");
 		} else {
 			System.out.println("Problema detectado!");
 		}
 
 		soma = calc.somar(10, 0);
 		if (soma == 10) {
-			System.out.println("Resultado " + soma + " est· correto!");
+			System.out.println("Resultado " + soma + " est√° correto!");
 		} else {
 			System.out.println("Problema detectado!");
 		}
 
 		soma = calc.somar(5, -5);
 		if (soma == 0) {
-			System.out.println("Resultado " + soma + " est· correto!");
+			System.out.println("Resultado " + soma + " est√° correto!");
 		} else {
 			System.out.println("Problema detectado!");
 		}
 
 		soma = calc.somar(-2, -3);
 		if (soma == -5) {
-			System.out.println("Resultado " + soma + " est· correto!");
+			System.out.println("Resultado " + soma + " est√° correto!");
 		} else {
 			System.out.println("Problema detectado!");
 		}
 
 		soma = calc.somar(2147483647, 1);
 		if (soma == 2147483648L) {
-			System.out.println("Resultado " + soma + " est· correto!");
+			System.out.println("Resultado " + soma + " est√° correto!");
 		} else {
 			System.out.println("Problema detectado!");
 		}
