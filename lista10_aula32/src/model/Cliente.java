8786b5a26e3d2f4d9f8e3e12bbad219e972c3585
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-17 18:44:13 -0300

    removido linhas e arraylist

--- lista10_aula32/src/model/Cliente.java
+++ lista10_aula32/src/model/Cliente.java
@@ -1,26 +1,19 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Cliente {
 	private String nome;
-	
-	ArrayList<ContaCorrente> cc;
-	ArrayList<ContaEspecial> ce;
-	
+		
 	public Cliente() {
 	}
 	
 	public Cliente(String nome) {
 		super();
 		this.nome = nome;
-		this.cc = new ArrayList<ContaCorrente>();
 	}
 	
 	public Cliente(String nome, double limite) {
 		super();
 		this.nome = nome;
-		this.ce = new ArrayList<ContaEspecial>();
 	}
 	
 	public void setNome(String nome) {
