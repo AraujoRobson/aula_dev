41ebd7bac0f5aeec3a5405e21eedccffbaca1f8c
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-17 19:18:00 -0300

    sobrescrito metodo para verificar limite

--- lista10_aula32/src/model/ContaEspecial.java
+++ lista10_aula32/src/model/ContaEspecial.java
@@ -10,10 +10,18 @@ public class ContaEspecial extends ContaCorrente {
 	
 	@Override
 	public void sacar(double valor) {
-		if(saldo >= valor) {
+		if(verificaSaque(valor)) {
 			saldo -= valor;
-		}else if((saldo + limite) >= valor) {
-			saldo -= valor;
+		}else {
+			super.erroSaque();
 		}
+	}
+	
+	@Override
+	public boolean verificaSaque(double valor) {
+		if((saldo + limite) >= valor) {
+			return true;
+		}
+		return false;
 	}
 }
