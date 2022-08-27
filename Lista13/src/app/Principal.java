ad5f0b4c3b902f25153bc172b2a6c557bf90d308
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-27 13:47:02 -0300

    format number and date

--- Lista13/src/app/Principal.java
+++ Lista13/src/app/Principal.java
@@ -1,12 +1,13 @@
 package app;
 
-import java.sql.Connection;
-import db.MySQLConnection;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		SimpleDateFormat fd = new SimpleDateFormat("dd.MM.yyyy");
+		NumberFormat fm = NumberFormat.getCurrencyInstance();
 	}
 
 }
