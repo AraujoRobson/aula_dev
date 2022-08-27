100e34e1609c7fae42371745b679dfd2303aadf7
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-27 13:27:20 -0300

    procedure listPeople

---
+++ Lista13/src/dao/PersonDAO.java
@@ -0,0 +1,58 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.MySQLConnection;
+import model.Person;
+
+public class PersonDAO implements IPersonDAO {
+	private Connection connection;
+	Person p = new Person();
+	
+	@Override
+	public List<Person> listPeople() {
+		connection = MySQLConnection.getConexao();
+		List<Person> list = new ArrayList<>();
+		
+		try {
+			String sql = "SELECT * FROM pessoa";
+			PreparedStatement stmt = this.connection.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				p.setIdPerson(rs.getInt("id_person"));
+				p.setNamePerson(rs.getString("name_person"));
+				p.setBirthDate(rs.getDate("birth_date"));
+				p.setSalary(rs.getBigDecimal("salary"));
+			}
+		}  catch(SQLException e) {
+			
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public void add(Person p) {
+	}
+
+	@Override
+	public void change(Person p) {
+	}
+
+	@Override
+	public void delete(Integer id) {
+	}
+
+
+	@Override
+	public Person search_id(Integer id) {
+		return null;
+	}
+
+}
