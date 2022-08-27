710b5e9b07d21d53ae84f1a0cbdd19c8ac6dbf04
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-27 13:40:30 -0300

    throws sql

--- Lista13/src/dao/IPersonDAO.java
+++ Lista13/src/dao/IPersonDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.sql.SQLException;
 import java.util.List;
 import model.Person;
 
@@ -7,6 +8,6 @@ public interface IPersonDAO {
 	void add(Person p);
 	void change(Person p);
 	void delete(Integer id);
-	List<Person> listPeople();	
+	List<Person> listPeople() throws SQLException;	
 	Person search_id(Integer id);
 }
