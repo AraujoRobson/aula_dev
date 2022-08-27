8930f480d9bbe2b86efc8b54a1882d9cdf64c29f
Robson Araujo <roobson.araujo02@gmail.com>
2022-08-27 10:19:50 -0300

    connection db

---
+++ Lista13/src/db/MySQLConnection.java
@@ -0,0 +1,39 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+	private static Connection connection = null;
+	
+	private MySQLConnection() {
+		
+	}
+	
+	public static Connection getConexao() {
+		try {
+			final String url = "jdbc:mysql://localhost/unoesc_trabalho_crud";
+			final String user = "root";
+			final String password = "root";
+			
+			connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Conexão realizada com sucesso");
+			
+			return connection;
+		} catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+	}
+	
+	public static void closeConnection() {
+		if (connection != null) {
+			try {
+				connection.close();
+				System.out.println("Conexão fechada com sucesso!");
+			} catch (SQLException e) {
+				throw new RuntimeException(e.getMessage());
+			}
+		}
+	}
+}
